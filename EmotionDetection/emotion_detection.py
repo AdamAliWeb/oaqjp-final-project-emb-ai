003a603ba50b9396673c46f98bf3a2a7a61633f0
AdamAliWeb <adamstar080904@gmail.com>
2026-02-11 11:21:45 -0500

    Task 3: Formated the function in order to return the emotions and the dominant emotion

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -7,4 +7,21 @@ def emotion_detector(text_to_analyse):
 
     response = requests.post(url, json=myobj, headers=header)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotion_scores = formatted_response["emotionPredictions"][0]["emotion"]
+    highest_score = 0
+    dominant_emotion = ""
+
+    for key, value in emotion_scores.items():
+        if value > highest_score:
+            dominant_emotion = key
+            highest_score = value
+
+    emotion_scores["dominant_emotion"] = dominant_emotion
+
+    return emotion_scores
+
+# from emotion_detection import emotion_detector
+
+# emotion_detector("I love this new technology.")
