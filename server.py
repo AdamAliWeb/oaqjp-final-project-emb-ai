ef80d48222fc5630944fcd6a416fdc660ccf6b98
AdamAliWeb <adamstar080904@gmail.com>
2026-02-11 15:23:58 -0500

    Task 8: Running pylint and implementing good code practices into server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" Main Server - Importing all required modules"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,27 +6,29 @@ app = Flask("EmotionDetectionApplication")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """ The function to process the text from the form and return the emotion scores"""
     text_to_analyze = request.args.get("textToAnalyze")
 
     response = emotion_detector(text_to_analyze)
 
     if response["dominant_emotion"] is None:
         return "Invalid text! Please try again!"
-    else:
-        dominant_emotion = response["dominant_emotion"]
-        del response["dominant_emotion"]
 
-        message = "For the given statement, the system response is "
+    dominant_emotion = response["dominant_emotion"]
+    del response["dominant_emotion"]
 
-        for key, value in response.items():
-            message += f"'{key}': {value}, "
+    message = "For the given statement, the system response is "
 
-        message = f"{message[0:-2]}. The dominant emotion is {dominant_emotion}."
+    for key, value in response.items():
+        message += f"'{key}': {value}, "
 
-        return message
+    message = f"{message[0:-2]}. The dominant emotion is {dominant_emotion}."
+
+    return message
 
 @app.route("/")
 def render_index_page():
+    """ Render the main page"""
     return render_template("index.html")
 
 if __name__ == "__main__":
